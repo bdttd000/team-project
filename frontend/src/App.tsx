b364623c36964cec51ac831bc403a03574693a4a
Jakub Wartak <kuba428@op.pl>
2023-11-24 23:31:22 +0100

    added basic react, ts and tailwind configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600 bg-blue-950">
+      Example
+    </h1>
+  );  
+}
+
+export default App;
