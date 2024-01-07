f4764ca5935aeb2d6051830010b4973b72cb0eb0
Jakub Wartak <kuba428@op.pl>
2024-01-07 11:03:08 +0100

    Added register page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -24,7 +24,7 @@ const Login: React.FC = () => {
       <div className='lg:w-1/2 flex flex-col justify-center content-center flex-wrap gap-6'>
         <img className='w-2/5' src={Logo} alt='Logo'></img>
         {/* <div className='text-center text-black font-bold text-5xl'>Zarejestruj się</div> */}
-        <div className='outline-none bg-blue-500 p-2 rounded-lg text-white cursor-pointer text-center text-xl' onClick={() => {navigate('register')}}> 
+        <div className='outline-none bg-blue-500 p-2 rounded-lg text-white cursor-pointer text-center text-xl' onClick={() => {navigate('/register')}}> 
           Zarejestruj się
         </div>
       </div>
